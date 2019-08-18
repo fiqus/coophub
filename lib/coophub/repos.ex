78ced2dcad93fc834ad9c8272b59932a0cd5ddf8
Diego Calero <dcalero@fiqus.coop>
2019-08-18 18:20:52 -0300

    Moving logic to get latest and popular repos into Coophub.Repos module and cleaning CoophubWeb.RepoController.

--- lib/coophub/repos.ex
+++ lib/coophub/repos.ex
@@ -2,6 +2,11 @@ defmodule Coophub.Repos do
   require Logger
 
   @repos_cache_name :repos_cache
+  @forks_factor 1.7
+  @stargazers_factor 1.5
+  @open_issues_factor 1.3
+  @fork_coeficient 0.5
+  @gravity 1.8
 
   @spec get_all_orgs :: map | :error
   def get_all_orgs() do
@@ -56,5 +61,69 @@ defmodule Coophub.Repos do
       %{"repos" => repos} -> repos
       err -> err
     end
+  end
+
+  @spec get_org_repos_latest(String.t(), integer) :: [map] | nil | :error
+  def get_org_repos_latest(org_name, qty) do
+    case get_org_repos(org_name) do
+      repos when is_list(repos) -> sort_and_take_repos(repos, &repo_pushed_at/1, qty)
+      err -> err
+    end
+  end
+
+  @spec get_org_repos_popular(String.t(), integer) :: [map] | nil | :error
+  def get_org_repos_popular(org_name, qty) do
+    case get_org_repos(org_name) do
+      repos when is_list(repos) -> sort_and_take_repos(repos, &repo_popularity/1, qty)
+      err -> err
+    end
+  end
+
+  @spec get_repos_latest(integer) :: [map] | nil | :error
+  def get_repos_latest(qty) do
+    case get_all_repos() do
+      repos when is_list(repos) -> sort_and_take_repos(repos, &repo_pushed_at/1, qty)
+      err -> err
+    end
+  end
+
+  @spec get_repos_popular(integer) :: [map] | nil | :error
+  def get_repos_popular(qty) do
+    case get_all_repos() do
+      repos when is_list(repos) -> sort_and_take_repos(repos, &repo_popularity/1, qty)
+      err -> err
+    end
+  end
+
+  defp sort_and_take_repos(repos, func, qty) do
+    repos
+    |> Enum.sort(&(func.(&1) >= func.(&2)))
+    |> Enum.take(qty)
+  end
+
+  defp repo_pushed_at(repo) do
+    {:ok, datetime, _} = DateTime.from_iso8601(repo["pushed_at"])
+    DateTime.to_unix(datetime)
+  end
+
+  # based on https://gist.github.com/soulim/d69e5dabc511c325f089
+  defp repo_popularity(repo) do
+    rating =
+      repo["stargazers_count"] * @stargazers_factor + repo["forks_count"] * @forks_factor +
+        repo["open_issues_count"] * @open_issues_factor
+
+    rating =
+      if repo["fork"],
+        do: rating * @fork_coeficient,
+        else: rating
+
+    {:ok, pushed_at_datetime, _} = DateTime.from_iso8601(repo["pushed_at"])
+
+    divisor =
+      (((DateTime.utc_now() |> DateTime.to_unix()) - (pushed_at_datetime |> DateTime.to_unix())) /
+         3600)
+      |> :math.pow(@gravity)
+
+    rating / divisor
   end
 end
--- lib/coophub_web/controllers/repo_controller.ex
+++ lib/coophub_web/controllers/repo_controller.ex
@@ -5,12 +5,6 @@ defmodule CoophubWeb.RepoController do
 
   action_fallback(CoophubWeb.FallbackController)
 
-  @forks_factor 1.7
-  @stargazers_factor 1.5
-  @open_issues_factor 1.3
-  @fork_coeficient 0.5
-  @gravity 1.8
-
   def index(conn, _) do
     case Repos.get_all_orgs() do
       :error -> render_status(conn, 500)
@@ -27,95 +21,32 @@ defmodule CoophubWeb.RepoController do
   end
 
   def org_repos_latest(conn, %{"name" => name}) do
-    case Repos.get_org_repos(name) do
-      :error ->
-        render_status(conn, 500)
-
-      nil ->
-        render_status(conn, 404)
-
-      repos ->
-        repos =
-          repos
-          |> Enum.sort(&dates_comparer/2)
-          |> Enum.take(3)
-
-        render(conn, "show.json", repos: repos)
+    case Repos.get_org_repos_latest(name, 3) do
+      :error -> render_status(conn, 500)
+      nil -> render_status(conn, 404)
+      repos -> render(conn, "show.json", repos: repos)
     end
   end
 
   def org_repos_popular(conn, %{"name" => name}) do
-    case Repos.get_org_repos(name) do
-      :error ->
-        render_status(conn, 500)
-
-      nil ->
-        render_status(conn, 404)
-
-      repos ->
-        repos =
-          repos
-          |> Enum.sort(&(repo_popularity(&1) >= repo_popularity(&2)))
-          |> Enum.take(3)
-
-        render(conn, "show.json", repos: repos)
+    case Repos.get_org_repos_popular(name, 3) do
+      :error -> render_status(conn, 500)
+      nil -> render_status(conn, 404)
+      repos -> render(conn, "show.json", repos: repos)
     end
   end
 
   def repos_latest(conn, _params) do
-    case Repos.get_all_repos() do
-      :error ->
-        render_status(conn, 500)
-
-      repos ->
-        repos =
-          repos
-          |> Enum.sort(&dates_comparer/2)
-          |> Enum.take(3)
-
-        render(conn, "show.json", repos: repos)
+    case Repos.get_repos_latest(3) do
+      :error -> render_status(conn, 500)
+      repos -> render(conn, "show.json", repos: repos)
     end
   end
 
   def repos_popular(conn, _params) do
-    case Repos.get_all_repos() do
-      :error ->
-        render_status(conn, 500)
-
-      repos ->
-        repos =
-          repos
-          |> Enum.sort(&(repo_popularity(&1) >= repo_popularity(&2)))
-          |> Enum.take(3)
-
-        render(conn, "show.json", repos: repos)
+    case Repos.get_repos_popular(3) do
+      :error -> render_status(conn, 500)
+      repos -> render(conn, "show.json", repos: repos)
     end
-  end
-
-  defp dates_comparer(repo1, repo2) do
-    {:ok, datetime1, _} = DateTime.from_iso8601(repo1["pushed_at"])
-    {:ok, datetime2, _} = DateTime.from_iso8601(repo2["pushed_at"])
-    DateTime.compare(datetime1, datetime2) === :gt
-  end
-
-  # based on https://gist.github.com/soulim/d69e5dabc511c325f089
-  defp repo_popularity(repo) do
-    rating =
-      repo["stargazers_count"] * @stargazers_factor + repo["forks_count"] * @forks_factor +
-        repo["open_issues_count"] * @open_issues_factor
-
-    rating =
-      if repo["fork"],
-        do: rating * @fork_coeficient,
-        else: rating
-
-    {:ok, pushed_at_datetime, _} = DateTime.from_iso8601(repo["pushed_at"])
-
-    divisor =
-      (((DateTime.utc_now() |> DateTime.to_unix()) - (pushed_at_datetime |> DateTime.to_unix())) /
-         3600)
-      |> :math.pow(@gravity)
-
-    rating / divisor
   end
 end
