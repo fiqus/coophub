0611707c0ae87e3d5898d757ead5b53b4efc75fa
Pablo Brudnick <pbrudnick@gmail.com>
2019-09-06 16:35:17 -0300

    #5 adding limit

--- lib/coophub_web/controllers/repo_controller.ex
+++ lib/coophub_web/controllers/repo_controller.ex
@@ -3,6 +3,8 @@ defmodule CoophubWeb.RepoController do
 
   require Logger
 
+  @default_repos_limit 3
+
   action_fallback(CoophubWeb.FallbackController)
 
   def index(conn, _) do
@@ -20,31 +22,49 @@ defmodule CoophubWeb.RepoController do
     end
   end
 
-  def org_repos_latest(conn, %{"name" => name}) do
-    case Repos.get_org_repos_latest(name, 3) do
+  def org_repos_latest(conn, %{"name" => name} = params) do
+    limit = get_limit(params)
+    case Repos.get_org_repos_latest(name, limit) do
       :error -> render_status(conn, 500)
       nil -> render_status(conn, 404)
       repos -> render(conn, "repos.json", repos: repos)
     end
   end
 
-  def org_repos_popular(conn, %{"name" => name}) do
-    case Repos.get_org_repos_popular(name, 3) do
+  def org_repos_popular(conn, %{"name" => name} = params) do
+    limit = get_limit(params)
+    case Repos.get_org_repos_popular(name, limit) do
       :error -> render_status(conn, 500)
       nil -> render_status(conn, 404)
       repos -> render(conn, "repos.json", repos: repos)
     end
   end
 
-  def repos_latest(conn, _params) do
-    case Repos.get_repos_latest(3) do
+  defp get_limit(%{"limit" => limit}) do
+    try do
+      String.to_integer(limit)
+    rescue
+      _ -> @default_repos_limit
+    end
+  end
+
+  defp get_limit(_), do: @default_repos_limit
+
+  def repos_latest(conn, params) do
+    params
+    |> get_limit()
+    |> Repos.get_repos_latest()
+    |> case do
       :error -> render_status(conn, 500)
       repos -> render(conn, "repos.json", repos: repos)
     end
   end
 
-  def repos_popular(conn, _params) do
-    case Repos.get_repos_popular(3) do
+  def repos_popular(conn, params) do
+    params
+    |> get_limit()
+    |> Repos.get_repos_popular()
+    |> case do
       :error -> render_status(conn, 500)
       repos -> render(conn, "repos.json", repos: repos)
     end
