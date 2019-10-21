cf7f0f150a3be968d1c5521c0c790d16728502e6
Hernán Gigena <hernangigena@fiqus.com>
2019-10-21 15:12:23 -0300

    Add style to porcentage languages

---
+++ assets/js/components/LanguageProgressBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Language} from "../types";
+import { Container } from 'reactstrap';
+
+const LanguageProgressBar:React.FC<{language: Language}> = ({language}) => {
+    /** @type {{search: React.CSSProperties}} */
+    const styles = {
+        width: `${language.percentage}%`
+    };
+    return (
+        <Container className="col-2">
+            <span> {language.lang} </span> 
+            <span class="pull-right"> {language.percentage} </span>
+            <span class="progress">
+                <span class="progress-bar" role="progressbar"  style= {styles} aria-valuenow={language.percentage} aria-valuemin="0" aria-valuemax="100"></span>
+            </span>
+        </Container>
+    );
+};
+
+export default LanguageProgressBar;
--- assets/js/components/OrgHeader.tsx
+++ assets/js/components/OrgHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GitHubButton from 'react-github-btn';
 import {Container, Jumbotron, ButtonGroup, Button, CardLink, Row, Col} from "reactstrap";
 import {GoLocation, GoLink, GoMail} from "react-icons/all";
-import LanguageTag from './LanguageTag'; 
+import LanguageProgressBar from './LanguageProgressBar'; 
 import {Org} from "../types";
 
 const OrgHeader:React.FC<{org: Org, maxLanguages: number}> = ({org, maxLanguages}) => {
@@ -21,31 +21,39 @@ const OrgHeader:React.FC<{org: Org, maxLanguages: number}> = ({org, maxLanguages
                 {org.description}
                 </p>
                 <div class="line-mf mb-3"></div>
-                <ButtonGroup>
-                    <Button color="link">
-                        <GoLocation/> { org.location }
-                    </Button>
-                    <Button color="link">
-                        <GoMail/> { org.email }
-                    </Button>
-                    <Button color="link"> 
-                        <CardLink href={org.blog}> 
-                            <GoLink/> { org.blog }
-                        </CardLink>
-                    </Button>
-                </ButtonGroup>
-                <br/>
-                <ButtonGroup>
-                    {mainLanguages.map(lang => <LanguageTag key={lang.lang} language={lang} />)}
-                </ButtonGroup>
-                <p className="mt-2">
-                    <GitHubButton  href={"https://github.com/" + org.login} data-size="large"
-                                data-show-count
-                                aria-label={"Follow @ " + org.login + " on GitHub"}>
-                    Follow @{org.login}
-                    </GitHubButton>
-                </p>
+                <Row>
+                    <Container>
+                        <div class="skill-mf">
+                            <Row>
+                                {mainLanguages.map(lang => <LanguageProgressBar key={lang.lang} language={lang} />)}
+                            </Row>
+                        </div>
+                        <ButtonGroup>
+                            <Button color="link">
+                                <GoLocation/> { org.location }
+                            </Button>
+                            <Button color="link">
+                                <GoMail/> { org.email }
+                            </Button>
+                            <Button color="link"> 
+                                <CardLink href={org.blog}> 
+                                    <GoLink/> { org.blog }
+                                </CardLink>
+                            </Button>
+                        </ButtonGroup>
+                        <br/>
+                        <p className="mt-2">
+                            <GitHubButton  href={"https://github.com/" + org.login} data-size="large"
+                                        data-show-count
+                                        aria-label={"Follow @ " + org.login + " on GitHub"}>
+                            Follow @{org.login}
+                            </GitHubButton>
+                        </p>
+                    </Container>
+                </Row>
+                
             </div>
+            
             
         </Container>
         
