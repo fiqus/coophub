2e68145a615d1ea848112b47f232cb303d89d439
mlambir <mlambir@fiqus.com>
2019-12-13 17:30:33 -0300

    add language per color to the coop progress bar

--- assets/js/components/LanguagesProgressBar.tsx
+++ assets/js/components/LanguagesProgressBar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Language} from "../types";
 import { Container } from 'reactstrap';
 import { HorizontalBar } from 'react-chartjs-2';
+import getLangColor from '../languageColors';
 
 const LanguagesProgressBar:React.FC<{languages: Array<Language>, maxLanguages: number}> = ({languages, maxLanguages}) => {
     let mainLanguages = languages.slice(0, maxLanguages);
@@ -16,7 +17,8 @@ const LanguagesProgressBar:React.FC<{languages: Array<Language>, maxLanguages: n
      mainLanguages.push({bytes: 0,lang: "other", percentage: 100.0 - (mainLanguages.reduce((acc, lang) => acc + lang.percentage, 0))})
      const data ={ 
        datasets: mainLanguages.map(i => {return {label: `${i.lang} (${Number(i.percentage).toFixed(2)}%)`, 
-                                                 barThickness: 10, 
+                                                 barThickness: 10,
+                                                 backgroundColor: getLangColor(i.lang),
                                                  data: [i.percentage]}}),
        labels:['languages']
      } 
