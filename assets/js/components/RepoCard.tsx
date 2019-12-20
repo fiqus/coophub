c04a54467ffdbf00f2906be5850c49800b8aee2d
mlambir <mlambir@fiqus.com>
2019-12-20 16:24:24 -0300

    add better emoji support

--- assets/js/components/RepoCard.tsx
+++ assets/js/components/RepoCard.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {Card, CardHeader, CardBody, CardLink, CardText, CardFooter, Button} from "reactstrap";
 import {GoRepoForked, GoStar} from "react-icons/all";
 import {Repo} from "../types";
-import Helpers from "../helpers";
 import getLangColor from '../languageColors';
+import Emoji from "react-emoji-render";
+
 
 const RepoCard:React.FC<{repo: Repo}> = ({repo}) => {
     const langs = repo.languages.filter(l => l.percentage > 30).sort((a,b)=>b.percentage - a.percentage).map(l => l.lang);
@@ -24,7 +25,10 @@ const RepoCard:React.FC<{repo: Repo}> = ({repo}) => {
                         </div>
                     </div>
                 : ""}
-                <CardText>{repo.fork ? "(fork) " : ""}{repo.description || <i>- no description -</i>}</CardText>
+                <CardText>
+                    {repo.fork ? "(fork) " : ""}
+                    {<Emoji text={repo.description}/> || <i>- no description -</i>}
+                </CardText>
             </CardBody>
             <CardFooter>
                 <div className="post-author">
