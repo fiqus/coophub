f54ed097e71a3856f0c31efe020c616380b79444
Diego Calero <dcalero@fiqus.coop>
2023-03-18 21:55:46 -0300

    Upgrading node+deps.

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -6,9 +6,10 @@ import '../css/app.css';
 import 'phoenix_html';
 
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import Root from './Root';
 
 // This code starts up the React app when it runs in a browser. It sets up the routing
 // configuration and injects the app into a DOM element.
-ReactDOM.render(<Root/>, document.getElementById('react-app'));
+const container = document.getElementById('react-app');
+ReactDOMClient.createRoot(container).render(<Root/>);
