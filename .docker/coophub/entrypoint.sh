2cbd4e399e1c9d1871272a73864dd0079108aede
Vitor Mattos <vitor@php.rio>
2023-05-31 11:01:45 -0300

    Document the entrypoint.sh file
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .docker/coophub/entrypoint.sh
+++ .docker/coophub/entrypoint.sh
@@ -4,16 +4,16 @@
 if [ -f ".env" ]; then
     . .env
 fi
-source $HOME/.bashrc
+source $HOME/.bashr
 
-# tail -f /dev/null
-
+# If you want to recreate deps, delete this folder and run docker compose up again
 if [ ! -d "deps" ]; then
     mix local.hex --force
     mix deps.get --force
     mix local.rebar --force
 fi
 
+# If you want to recreate node_modules, delete this folder and run docker compose up again
 if [ ! -d "node_modules" ]; then
     npm ci
 fi
