1dd7a5c0a8a0724a6decb4ccf625994c6510a61a
Vitor Mattos <vitor@php.rio>
2023-05-31 13:01:13 -0300

    Dockerize the project (#141)
    * Dockerize
    
    To make more easy to run the development environment by developers that
    don't know a lot of elixir or developers that don't want to install
    dependencies locally, now will be possible execute the project using
    `docker compose`.
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>
    
    * Make possible customize the elixir version
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>
    
    * Document the entrypoint.sh file
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>
    
    * Run node in a isolated service
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>
    
    * Simplify the project
    
    Removed Dockerfile because isn't more necessary
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>
    
    ---------
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>
    Co-authored-by: Diego Calero <dcalero@fiqus.coop>

---
+++ .docker/coophub/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Source the enviroment variables
+if [ -f ".env" ]; then
+    . .env
+fi
+source $HOME/.bashrc
+
+# If you want to recreate deps, delete this folder and run docker compose up again
+if [ ! -d "deps" ]; then
+    mix local.hex --force
+    mix deps.get --force
+    mix local.rebar --force
+fi
+
+mix phx.server
---
+++ .docker/nodejs/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Source the enviroment variables
+if [ -f ".env" ]; then
+    . .env
+fi
+source $HOME/.bashrc
+
+# If you want to recreate node_modules, delete this folder and run docker compose up again
+if [ ! -d "node_modules" ]; then
+    npm ci
+fi
+
+npm run watch
